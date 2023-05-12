77453621f2d26edf4dca73aa5be94150cc806192
tasika16 <tasika696@gmail.com>
2023-05-12 20:02:12 +0200

    * NEW: Paginator is working

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,37 +1,36 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatTableDataSource } from "@angular/material/table";
+
 import { CITIES, City } from "../data/cities";
 import { SearchService, WeatherRow } from "./search.service";
-import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: "search",
   templateUrl: "./search.component.html",
   styleUrls: ["./search.component.css"]
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
   cities: City[] = CITIES;
   results: WeatherRow[] = [];
   selected_city :City = this.cities[0];
 
+  length :number = 10;
   displayedColumns :string[] = [ 'date', 'time', 'temp' ]
+  dataSource = new MatTableDataSource<WeatherRow>([]);
 
-  dataSource :any;
+  @ViewChild(MatPaginator) paginator :MatPaginator;
 
   constructor(private service: SearchService) {
   }
 
-  public ngOnInit(): void {
-  }
-
   public updateResults(city: City): void {
-    console.log('Cica')
     // TODO set results
-    // this.results = this.service.getWeatherByCity(city);
-    this.dataSource = new MatTableDataSource<WeatherRow>(this.service.getWeatherByCity(city))
-  }
-
-  public handlePageEvent(e :any) {
+    this.results = this.service.getWeatherByCity(city);
+    this.length = this.results.length
+    this.dataSource.data = this.results;
+    this.dataSource.paginator = this.paginator;
 
   }
 }
